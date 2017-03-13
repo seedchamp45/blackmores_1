855402da38663fc4423451abd1144abdc5d7e329
chinoros <chinoros.x@gmail.com>
2017-03-13 10:48:40 +0700

    add hello

--- page/rule.php
+++ page/rule.php
@@ -1,10 +1,235 @@
 <html>
-<body>
-    <div class="row">
-        <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12 ">
-            <img src="images/rule/lues.png" class="img-responsive center-block ">  
+    <head>
+        <style>
+            .txt_r{
+               font-family: "db_helvethaica_x";
+          
+               display: block;
             
+            }
+            
+            .r_title{
+                color: #1677BC;
+                font-size: 2.7em;
+                font-weight: bold;  
+            }
+            
+            .r_body{
+                color: #000;
+                font-size:1.8em;
+                font-weight: normal;
+                display: inline;
+            }
+            
+            .r_bold{
+                color: #000;
+                font-size: 2em; 
+            }
+            ul ul, ol ul,ul {
+                list-style-type: none;
+            }
+            
+            .ul_left{
+              padding-left: 25px;
+            }
+            
+            li .a:before {
+                content: "•"; /* Insert content that looks like bullets */
+                padding-right: 8px;
+                
+            }
+            .ol_left{
+                margin-left: -22px;
+            }
+            
+            .bg_block{
+                background-image: url("images/rule/r_block_bg.png") ;
+                background-repeat: no-repeat;
+                background-size: 100% 100%;
+                background-position: center top;
+
+            }
+           
+          
+        </style>
+    </head>
+    <body>
+    <div class="row bg_block">
+        <div class="col-xs-1 col-sm-3 col-md-3 col-lg-3 "></div>
+        <div class="col-xs-10 col-sm-6 col-md-6 col-lg-6 ">
+            
+             <br><br><br><br>
+            <img src="images/rule/r_title.png" class="img-responsive center-block">
+            <br/><br/>
+            <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+                <div class="txt_r r_title">ระยะเวลาการร่วมสนุก</div>
+                <ol class="ol_left">
+                    <li>
+                        <div class="txt_r r_body">ผู้ร่วมสนุกสามารถส่งผลงานเข้าประกวดได้ตั้งแต่วันที่ 13 มีนาคม– 24 เมษายน 2560 (รวมระยะเวลาทั้งสิ้น 43 วัน) 
+                    ผ่านทาง www.BlackmoresMyStrongFamily.com</div>
+                    </li>
+                     <li>
+                        <div class="txt_r r_body">ปิดรับผลงานทั้งหมดในวันที่ 24 เมษายน 2560 เวลา 24.00 น.</div>
+                    </li>
+                </ol>
+                
+                
+              
+                <br/><br/>
+                <div class="txt_r r_title">การประกาศรางวัล</div>
+                 <ol class="ol_left">
+                     <li>
+                         <div class="txt_r r_body">ประกาศผลวันที่ 28 เมษายน 2560 ผ่านทาง www.blackmoresmystrongfamily.com ดังนี้</div>
+                         <ul>
+                            <li><div class="txt_r r_body">- ผู้ผ่านเข้ารอบ รุ่นอายุ 8 – 12 ปี เข้าร่วม Workshop กับอ.ชลิต นาคพะวัน ในวันอาทิตย์ที่ 14 พฤษภาคม 2560
+              ที่โรงเรียนสอนศิลปะ Chalit Art Project and Gallery เพื่อหาผู้ชนะเลิศ จำนวน 30 คน</div></li>
+                           </ul>
+                     </li>
+                     <li>
+                         <div class="txt_r r_body">ประกาศผลวันที่ 17 พฤษภาคม 2560 ผ่านทาง www.blackmoresmystrongfamily.com ดังนี้ </div>
+                         <ul>
+                             <li>
+                                 <div class="txt_r r_body">&nbsp;&nbsp;&nbsp;&nbsp;ผู้ชนะเลิศประเภทอายุน้อยกว่า 8 ปี จำนวน 5 รางวัล</div>
+                             </li>  
+                                <div class="txt_r r_body">- ผู้ชนะเลิศประเภทอายุ 8- 12 ปี จำนวน 5 รางวัล</div>
+                            <li>  
+                                <div class="txt_r r_body">- ผู้ชนะ Popular Vote 10 รางวัล (รุ่นอายุละ 5 รางวัล)</div>
+                            </li>
+                            <li>
+                                <div class="txt_r r_body"> - ผู้โชคดีที่ร่วมโหวต จำนวน 10 รางวัล</div>
+                            </li>
+                        </ul>
+                     </li> 
+                 </ol>
+              
+                <br/><br/>
+                <div class="txt_r r_title">รางวัลในแต่ละประเภท</div> 
+                <div class="txt_r r_bold">&nbsp;&nbsp;&nbsp;&nbsp;รางวัลประเภทอายุน้อยกว่า 8 ปี</div>
+                <ul class="ul_left">
+                    <li>
+                        <div class="txt_r r_body">รางวัลทุนการศึกษารางวัลละ 3,000 บาท พร้อมผลิตภัณฑ์เสริมอาหาร Blackmores  มูลค่ารางวัลละ 10,000 บาท 5 รางวัล
+                    รวมมูลค่าทั้งสิ้น 65,000 บาท </div>
+                    </li>
+                </ul>
+
+                <div class="txt_r r_bold">&nbsp;&nbsp;&nbsp;&nbsp;รางวัลประเภทอายุ 8 – 12 ปี</div> 
+                <ul class="ul_left">
+                    <li>
+                        <div class="txt_r r_body">รางวัลทุนการศึกษารางวัลละ 5,000 บาท พร้อมผลิตภัณฑ์เสริมอาหาร Blackmores  มูลค่ารางวัลละ 10,000 บาท 5 รางวัล
+                    รวมมูลค่าทั้งสิ้น 75,000 บาท </div>  
+                    </li>
+                </ul>
+                <div class="txt_r r_bold">&nbsp;&nbsp;&nbsp;&nbsp;รางวัลผลงาน Popular Vote</div> 
+                <ul class="ul_left">
+                    <li>
+                        <div class="txt_r r_body">รางวัลผลิตภัณฑ์เสริมอาหาร Blackmores มูลค่ารางวัลละ 3,000 บาท 10 รางวัล รวมมูลค่าทั้งสิ้น 30,000 บาท</div> 
+                    </li>
+                </ul>
+               
+                <div class="txt_r r_bold">&nbsp;&nbsp;&nbsp;&nbsp;รางวัลผู้ที่เข้าร่วมโหวต</div> 
+                <ul class="ul_left">
+                    <li>
+                        <div class="txt_r r_body">รางวัลผลิตภัณฑ์เสริมอาหาร Blackmores มูลค่ารางวัลละ 2,000 บาท 10 รางวัล รวมมูลค่าทั้งสิ้น 20,000 บาท</div> 
+                    </li>
+                </ul>
+                
+                <br/><br/>
+                <div class="txt_r r_title">เกณฑ์การตัดสิน </div> 
+                <div class="txt_r r_body">ตัดสินรางวัลจากทีมคณะกรรมการ บริษัท แบลคมอร์ส จำกัด และอาจารย์ชลิต นาคพะวัน</div> 
+                <br/>
+                <br/>
+                
+                <div class="txt_r r_title">เงื่อนไขการรับของรางวัล </div> 
+                 <ol class="ol_left">
+                     <li> 
+                         <div class="txt_r r_body">
+                         ผู้โชคดีที่ได้รับรางวัล ทุนการศึกษา และ ผลิตภัณฑ์เสริมอาหาร ต้องมี “ผลงานที่ส่งเข้าประกวด” และ ชื่อ-นามสกุล ที่ระบุในผลงานซึ่งตรงกับประกาศผู้ชนะเลิศทางเว็บไซต์ พร้อมเอกสารยืนยันตัวตน มาแสดงเป็นหลักฐานในการรับของรางวัล
+                         </div>
+                    </li>
+                    <li> 
+                        <div class="txt_r r_body">
+                         ผู้ที่ได้รับคัดเลือกเข้าร่วม Workshop 30 คน จะต้องยืนยันสิทธิ์ภายใน 7 วัน (ตั้งแต่วันที่ 29 เมษายน – 9 พฤษภาคม) ผ่านทาง Inbox ของ Facebook Blackmores Thailand นับตั้งแต่วันประกาศรายชื่อ
+                         </div>
+                    </li>
+                    <li>
+                         <div class="txt_r r_body">
+                        ผู้โชคดีที่ได้รับรางวัลทุนการศึกษา และผลิตภัณฑ์เสริมอาหารจะต้องยืนยันสิทธิ์ในการรับรางวัลภายใน 7 วัน (วันที่ 19 – 24 พฤษภาคม 2560) ผ่านทาง Inbox ของ Facebook Blackmores Thailand นับตั้งแต่วันประกาศรายชื่อ
+                        </div>
+                    </li>
+                    <li>
+                        <div class="txt_r r_body">
+                        เกณฑ์การตัดสินรางวัลมีดังนี้
+                        </div>
+                        <ul>
+                            <li class="a">
+                                <div class="txt_r r_body">
+                                สำหรับประเภทอายุน้อยกว่า 8 ปี ทางคณะกรรมการจะตัดสินจากผลงานที่สร้างสรรค์ที่สุด
+                                </div>
+                            </li> 
+                            <li class="a">
+                                <div class="txt_r r_body">
+                                สำหรับประเภทอายุ 8-12 ปี ทางคณะกรรมการจะตัดสินจากผลงานที่สร้างสรรค์ที่สุด 30 รางวัล และร่วมเวิร์คช้อปอีกครั้งจึงประกาศ รางวัลผู้ชนะเลิศ 5 รางวัล
+                                </div>
+                            </li>
+                            <li class="a">
+                                <div class="txt_r r_body">
+                                สำหรับผลงาน Popular Vote ทางคณะกรรมการจะตัดสินจากคะแนนที่มากที่สุด 5 อันดับแรกของแต่ละประเภทอายุ รวมเป็น 10 รางวัล
+                                </div>
+                            </li>
+                            <li class="a">
+                                <div class="txt_r r_body">
+                                สำหรับผู้เข้าร่วมโหวตผลงาน ทางคณะกรรมการจะตัดสินด้วยวิธีการสุ่มผู้โชคดี
+                                </div>
+                            </li>
+                        </ul>
+                    </li>
+                    <li>
+                        <div class="txt_r r_body">
+                        สงวนสิทธิ์ รับรางวัลมูลค่าสูงสุดเพียงรางวัลเดียวเท่านั้น ต่อ 1 ท่าน อาทิเช่น หากท่านชนะเลิศในรายการประเภทอายุต่ำกว่า 8 ปี และชนะเลิศรางวัล Popular Vote ด้วยทางทีมงานจะขอสงวนสิทธิ์ให้เพียงรางวัลเดียว ที่มีมูลค่าสูงสุดเท่านั้น
+                        </div>
+                    </li>
+                    <li>
+                        <div class="txt_r r_body">
+                        ผู้ชนะเลิศทั้งประเภทอายุน้อยกว่า 8 ปี และประเภทอายุ 8-12 ปี ต้องมารับรางวัลที่ บริษัท แบลคมอร์ส จำกัด ที่อยู่ ชั้น 21 เอ อาคารมหานครยิบซั่ม 539/2 ถ.ศรีอยุธยา แขวงถนนพญาไท เขตราชเทวี กรุงเทพฯ 10400 ด้วยตัวเองเท่านั้น ผู้ชนะเลิศรางวัล Popular vote และผู้โชคดีจากการร่วมโหวตนั้น ทาง บริษัท แบลคมอร์ส จำกัด จะจัดส่งของรางวัลไปให้ทางไปรษณีย์
+                        </div>
+                    </li> 
+                    <li>
+                        <div class="txt_r r_body">
+                        รางวัลไม่สามารถแลกทอนหรือเปลี่ยนเป็นเงินสดได้
+                        </div>
+                    </li>
+                    <li>
+                        <div class="txt_r r_body">
+                        การตัดสินจากทีมคณะกรรมการบริษัท แบลคมอร์ส จำกัด และอาจารย์ชลิต นาคพะวัน ถือเป็นเด็ดขาดและสิ้นสุด
+                        </div>
+                    </li>
+                    <li>
+                        <div class="txt_r r_body">
+                        ผลงานการประกวดของผู้ชนะถือเป็นลิขสิทธิ์ของ Blackmores ในการครอบครองแต่เพียงผู้เดียว
+                        </div>
+                    </li>
+                    <li>
+                        <div class="txt_r r_body">
+                        พนักงานและครอบครัวของ บริษัท แบลคมอร์ส จำกัด ตัวแทนโฆษณา รวมทั้งคณะกรรมการดำเนินงานพร้อมครอบครัวไม่สามารถเข้าร่วมกิจกรรมได้
+                        </div>
+                    </li> 
+                    <li>
+                        <div class="txt_r r_body">
+                        หากคณะกรรมการตรวจพบว่าผลงานที่ส่งเข้าประกวดมีการลอกเลียนแบบผลงานของผู้อื่น ทางบริษัท แบลคมอร์ส จำกัด จะทำการตัดสิทธิ์ และจะไม่รับผิดชอบการฟ้องร้องใดๆ ทั้งสิ้น
+                        </div>
+                    </li>
+                    <li> 
+                        <div class="txt_r r_body">
+                        หากของรางวัลมีมูลค่าตั้งแต่ 1,000 บาท ขึ้นไป ผู้ได้รับของรางวัลต้องเสียภาษีหัก ณ ที่จ่าย 5 %
+                        </div>
+                    </li>
+                 </ol>
+                <br><br><br><br><br><br><br><br>
+            </div>
         </div>
+        <div class="col-xs-1 col-sm-3 col-md-3 col-lg-3 "></div>
+        
+        <br><br><br><br><br><br><br><br>
     </div>
 </body>
 </html>
