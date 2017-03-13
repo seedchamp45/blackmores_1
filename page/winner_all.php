e81a2f375ba4b6dac31f37565a798d7fe6edb12c
chinoros <chinoros.x@gmail.com>
2017-03-13 21:34:12 +0700

    แก้ไขปุ่ม

--- page/winner_all.php
+++ page/winner_all.php
@@ -21,7 +21,7 @@
 .with-nav-tabs.panel-default .nav-tabs > li > a,
 .with-nav-tabs.panel-default .nav-tabs > li > a:hover,
 .with-nav-tabs.panel-default .nav-tabs > li > a:focus {
-    color: #777;
+    color: #bce8f1;
 }
 .with-nav-tabs.panel-default .nav-tabs > .open > a,
 .with-nav-tabs.panel-default .nav-tabs > .open > a:hover,
@@ -57,210 +57,6 @@
     color: #fff;
     background-color: #555;
 }
-/********************************************************************/
-/*** PANEL PRIMARY ***/
-.with-nav-tabs.panel-primary .nav-tabs > li > a,
-.with-nav-tabs.panel-primary .nav-tabs > li > a:hover,
-.with-nav-tabs.panel-primary .nav-tabs > li > a:focus {
-    color: #fff;
-}
-.with-nav-tabs.panel-primary .nav-tabs > .open > a,
-.with-nav-tabs.panel-primary .nav-tabs > .open > a:hover,
-.with-nav-tabs.panel-primary .nav-tabs > .open > a:focus,
-.with-nav-tabs.panel-primary .nav-tabs > li > a:hover,
-.with-nav-tabs.panel-primary .nav-tabs > li > a:focus {
-	color: #fff;
-	background-color: #3071a9;
-	border-color: transparent;
-}
-.with-nav-tabs.panel-primary .nav-tabs > li.active > a,
-.with-nav-tabs.panel-primary .nav-tabs > li.active > a:hover,
-.with-nav-tabs.panel-primary .nav-tabs > li.active > a:focus {
-	color: #428bca;
-	background-color: #fff;
-	border-color: #428bca;
-	border-bottom-color: transparent;
-}
-.with-nav-tabs.panel-primary .nav-tabs > li.dropdown .dropdown-menu {
-    background-color: #428bca;
-    border-color: #3071a9;
-}
-.with-nav-tabs.panel-primary .nav-tabs > li.dropdown .dropdown-menu > li > a {
-    color: #fff;   
-}
-.with-nav-tabs.panel-primary .nav-tabs > li.dropdown .dropdown-menu > li > a:hover,
-.with-nav-tabs.panel-primary .nav-tabs > li.dropdown .dropdown-menu > li > a:focus {
-    background-color: #3071a9;
-}
-.with-nav-tabs.panel-primary .nav-tabs > li.dropdown .dropdown-menu > .active > a,
-.with-nav-tabs.panel-primary .nav-tabs > li.dropdown .dropdown-menu > .active > a:hover,
-.with-nav-tabs.panel-primary .nav-tabs > li.dropdown .dropdown-menu > .active > a:focus {
-    background-color: #4a9fe9;
-}
-/********************************************************************/
-/*** PANEL SUCCESS ***/
-.with-nav-tabs.panel-success .nav-tabs > li > a,
-.with-nav-tabs.panel-success .nav-tabs > li > a:hover,
-.with-nav-tabs.panel-success .nav-tabs > li > a:focus {
-	color: #3c763d;
-}
-.with-nav-tabs.panel-success .nav-tabs > .open > a,
-.with-nav-tabs.panel-success .nav-tabs > .open > a:hover,
-.with-nav-tabs.panel-success .nav-tabs > .open > a:focus,
-.with-nav-tabs.panel-success .nav-tabs > li > a:hover,
-.with-nav-tabs.panel-success .nav-tabs > li > a:focus {
-	color: #3c763d;
-	background-color: #d6e9c6;
-	border-color: transparent;
-}
-.with-nav-tabs.panel-success .nav-tabs > li.active > a,
-.with-nav-tabs.panel-success .nav-tabs > li.active > a:hover,
-.with-nav-tabs.panel-success .nav-tabs > li.active > a:focus {
-	color: #3c763d;
-	background-color: #fff;
-	border-color: #d6e9c6;
-	border-bottom-color: transparent;
-}
-.with-nav-tabs.panel-success .nav-tabs > li.dropdown .dropdown-menu {
-    background-color: #dff0d8;
-    border-color: #d6e9c6;
-}
-.with-nav-tabs.panel-success .nav-tabs > li.dropdown .dropdown-menu > li > a {
-    color: #3c763d;   
-}
-.with-nav-tabs.panel-success .nav-tabs > li.dropdown .dropdown-menu > li > a:hover,
-.with-nav-tabs.panel-success .nav-tabs > li.dropdown .dropdown-menu > li > a:focus {
-    background-color: #d6e9c6;
-}
-.with-nav-tabs.panel-success .nav-tabs > li.dropdown .dropdown-menu > .active > a,
-.with-nav-tabs.panel-success .nav-tabs > li.dropdown .dropdown-menu > .active > a:hover,
-.with-nav-tabs.panel-success .nav-tabs > li.dropdown .dropdown-menu > .active > a:focus {
-    color: #fff;
-    background-color: #3c763d;
-}
-/********************************************************************/
-/*** PANEL INFO ***/
-.with-nav-tabs.panel-info .nav-tabs > li > a,
-.with-nav-tabs.panel-info .nav-tabs > li > a:hover,
-.with-nav-tabs.panel-info .nav-tabs > li > a:focus {
-	color: #31708f;
-}
-.with-nav-tabs.panel-info .nav-tabs > .open > a,
-.with-nav-tabs.panel-info .nav-tabs > .open > a:hover,
-.with-nav-tabs.panel-info .nav-tabs > .open > a:focus,
-.with-nav-tabs.panel-info .nav-tabs > li > a:hover,
-.with-nav-tabs.panel-info .nav-tabs > li > a:focus {
-	color: #31708f;
-	background-color: #bce8f1;
-	border-color: transparent;
-}
-.with-nav-tabs.panel-info .nav-tabs > li.active > a,
-.with-nav-tabs.panel-info .nav-tabs > li.active > a:hover,
-.with-nav-tabs.panel-info .nav-tabs > li.active > a:focus {
-	color: #31708f;
-	background-color: #fff;
-	border-color: #bce8f1;
-	border-bottom-color: transparent;
-}
-.with-nav-tabs.panel-info .nav-tabs > li.dropdown .dropdown-menu {
-    background-color: #d9edf7;
-    border-color: #bce8f1;
-}
-.with-nav-tabs.panel-info .nav-tabs > li.dropdown .dropdown-menu > li > a {
-    color: #31708f;   
-}
-.with-nav-tabs.panel-info .nav-tabs > li.dropdown .dropdown-menu > li > a:hover,
-.with-nav-tabs.panel-info .nav-tabs > li.dropdown .dropdown-menu > li > a:focus {
-    background-color: #bce8f1;
-}
-.with-nav-tabs.panel-info .nav-tabs > li.dropdown .dropdown-menu > .active > a,
-.with-nav-tabs.panel-info .nav-tabs > li.dropdown .dropdown-menu > .active > a:hover,
-.with-nav-tabs.panel-info .nav-tabs > li.dropdown .dropdown-menu > .active > a:focus {
-    color: #fff;
-    background-color: #31708f;
-}
-/********************************************************************/
-/*** PANEL WARNING ***/
-.with-nav-tabs.panel-warning .nav-tabs > li > a,
-.with-nav-tabs.panel-warning .nav-tabs > li > a:hover,
-.with-nav-tabs.panel-warning .nav-tabs > li > a:focus {
-	color: #8a6d3b;
-}
-.with-nav-tabs.panel-warning .nav-tabs > .open > a,
-.with-nav-tabs.panel-warning .nav-tabs > .open > a:hover,
-.with-nav-tabs.panel-warning .nav-tabs > .open > a:focus,
-.with-nav-tabs.panel-warning .nav-tabs > li > a:hover,
-.with-nav-tabs.panel-warning .nav-tabs > li > a:focus {
-	color: #8a6d3b;
-	background-color: #faebcc;
-	border-color: transparent;
-}
-.with-nav-tabs.panel-warning .nav-tabs > li.active > a,
-.with-nav-tabs.panel-warning .nav-tabs > li.active > a:hover,
-.with-nav-tabs.panel-warning .nav-tabs > li.active > a:focus {
-	color: #8a6d3b;
-	background-color: #fff;
-	border-color: #faebcc;
-	border-bottom-color: transparent;
-}
-.with-nav-tabs.panel-warning .nav-tabs > li.dropdown .dropdown-menu {
-    background-color: #fcf8e3;
-    border-color: #faebcc;
-}
-.with-nav-tabs.panel-warning .nav-tabs > li.dropdown .dropdown-menu > li > a {
-    color: #8a6d3b; 
-}
-.with-nav-tabs.panel-warning .nav-tabs > li.dropdown .dropdown-menu > li > a:hover,
-.with-nav-tabs.panel-warning .nav-tabs > li.dropdown .dropdown-menu > li > a:focus {
-    background-color: #faebcc;
-}
-.with-nav-tabs.panel-warning .nav-tabs > li.dropdown .dropdown-menu > .active > a,
-.with-nav-tabs.panel-warning .nav-tabs > li.dropdown .dropdown-menu > .active > a:hover,
-.with-nav-tabs.panel-warning .nav-tabs > li.dropdown .dropdown-menu > .active > a:focus {
-    color: #fff;
-    background-color: #8a6d3b;
-}
-/********************************************************************/
-/*** PANEL DANGER ***/
-.with-nav-tabs.panel-danger .nav-tabs > li > a,
-.with-nav-tabs.panel-danger .nav-tabs > li > a:hover,
-.with-nav-tabs.panel-danger .nav-tabs > li > a:focus {
-	color: #a94442;
-}
-.with-nav-tabs.panel-danger .nav-tabs > .open > a,
-.with-nav-tabs.panel-danger .nav-tabs > .open > a:hover,
-.with-nav-tabs.panel-danger .nav-tabs > .open > a:focus,
-.with-nav-tabs.panel-danger .nav-tabs > li > a:hover,
-.with-nav-tabs.panel-danger .nav-tabs > li > a:focus {
-	color: #a94442;
-	background-color: #ebccd1;
-	border-color: transparent;
-}
-.with-nav-tabs.panel-danger .nav-tabs > li.active > a,
-.with-nav-tabs.panel-danger .nav-tabs > li.active > a:hover,
-.with-nav-tabs.panel-danger .nav-tabs > li.active > a:focus {
-	color: #a94442;
-	background-color: #fff;
-	border-color: #ebccd1;
-	border-bottom-color: transparent;
-}
-.with-nav-tabs.panel-danger .nav-tabs > li.dropdown .dropdown-menu {
-    background-color: #f2dede; /* bg color */
-    border-color: #ebccd1; /* border color */
-}
-.with-nav-tabs.panel-danger .nav-tabs > li.dropdown .dropdown-menu > li > a {
-    color: #a94442; /* normal text color */  
-}
-.with-nav-tabs.panel-danger .nav-tabs > li.dropdown .dropdown-menu > li > a:hover,
-.with-nav-tabs.panel-danger .nav-tabs > li.dropdown .dropdown-menu > li > a:focus {
-    background-color: #ebccd1; /* hover bg color */
-}
-.with-nav-tabs.panel-danger .nav-tabs > li.dropdown .dropdown-menu > .active > a,
-.with-nav-tabs.panel-danger .nav-tabs > li.dropdown .dropdown-menu > .active > a:hover,
-.with-nav-tabs.panel-danger .nav-tabs > li.dropdown .dropdown-menu > .active > a:focus {
-    color: #fff; /* active text color */
-    background-color: #a94442; /* active bg color */
-}
 
 .bg_winner{
     /*background-image: url("images/winner/bg_winner.png");*/
@@ -279,36 +75,15 @@
 
 
 .panel {
-    margin-bottom: 20px;
-    border: 0px solid transparent;
-    border-radius: 4px;
-    -webkit-box-shadow: 0 1px 1px rgba(0, 0, 0, .05);
-    box-shadow: 0 1px 1px rgba(0, 0, 0, .05);
-}
-
-.full_bg{
-    position: fixed;
-    top: 0;
-    left: 0;
-    min-width: 90%;
-    min-height: 90%;
-    height: 2500px;
-    z-index: -99;
-    background: url(images/gallery/bg_winner.png) no-repeat center center fixed;
-    -webkit-background-size: cover;
-    -moz-background-size: cover;
-    -o-background-size: cover;
-    background-size: cover;
-}
-
-.panel {
-    margin-bottom: 20px;
+      margin-bottom: 20px;
     background-color: rgba(255, 255, 255, 0);
     border: 1px solid transparent;
     border-radius: 4px;
     -webkit-box-shadow: 0 1px 1px rgba(0, 0, 0, .05);
     box-shadow: 0 1px 1px rgba(0, 0, 0, .05);
 }
+
+
 
 .panel-default > .panel-heading {
     color: #333;
@@ -372,7 +147,7 @@
 .nav-tabs.centered > li, .nav-pills.centered > li {
     float:inherit;
     display:inline-block;
-    *display:inline; /* ie7 fix */
+    display:inline; /* ie7 fix */
      zoom:1; /* hasLayout ie7 trigger */
 }
 
@@ -393,7 +168,11 @@
 }
 
 
-.with-nav-tabs.panel-default .nav-tabs > .open > a, .with-nav-tabs.panel-default .nav-tabs > .open > a:hover, .with-nav-tabs.panel-default .nav-tabs > .open > a:focus, .with-nav-tabs.panel-default .nav-tabs > li > a:hover, .with-nav-tabs.panel-default .nav-tabs > li > a:focus {
+.with-nav-tabs.panel-default .nav-tabs > .open > a, 
+.with-nav-tabs.panel-default .nav-tabs > .open > a:hover, 
+.with-nav-tabs.panel-default .nav-tabs > .open > a:focus,
+.with-nav-tabs.panel-default .nav-tabs > li > a:hover, 
+.with-nav-tabs.panel-default .nav-tabs > li > a:focus {
     color: #000;
     background-color: rgba(0, 0, 0, 0);
     border-color: transparent;
@@ -425,37 +204,77 @@
     margin-bottom: -1px;
     border: 0;
 }
+.panel-body {
+    padding: 30px;
+}
+ul.nav-tabs {
+  text-align: center;
+}
+ul.nav-tabs li {
+  float: none;
+  display: inline-block;
+}
+.c_h{
+    position: relative;
+    top: 50%;
+    transform: translateY(-50%);
+}
   </style>
 </head>
 <body>
  <!--<div id="full_bg"></div>-->
  
  <div class="container">
-   <div class="row bg_winner">
-        <div class="col-xs-12 col-sm-12 col-md-1 col-lg-2 "> </div>
-        <div class="col-xs-12 col-sm-12 col-md-10 col-lg-8 ">
-            <div class="panel with-nav-tabs panel-default center-block">
-                <div class="panel-heading ">
-
-                        <ul class="nav nav-tabs ul-center center-block">
-                            <li class="icon_yellow_winner"><a href="#tab1default" data-toggle="tab"><div class="txt_menu_winner">ประเภทอายุไม่เกิน 8 ปี</div> </a></li>
-                            <li class="icon_blue_winner"><a href="#tab2default" data-toggle="tab"><div class="txt_menu_winner">ประเภทอายุ 8-12 ปี </div></a></li>
-                            <li class="icon_green_winner"><a href="#tab3default" data-toggle="tab"><div class="txt_menu_winner">รายชื่อผู้ชนะ Popular Vote </div></a></li>
-                             <li class="icon_purple_winner"><a href="#tab4default" data-toggle="tab"><div class="txt_menu_winner">ผู้โชคดีจากการโหวต</div></a></li>
-                        </ul>
-                </div>
-                <div class="panel-body">
-                    <div class="tab-content">
-                        <div class="tab-pane fade in active" id="tab1default"><div class="txt_body_winner"> Default 111<br><br><br><br><br></div></div>
-                        <div class="tab-pane fade" id="tab2default"><div class="txt_body_winner"> Default 2</div></div>
-                        <div class="tab-pane fade" id="tab3default"><div class="txt_body_winner"> Default 3</div></div>
-                        <div class="tab-pane fade" id="tab4default"><div class="txt_body_winner"> Default 4</div></div>
-                        <div class="tab-pane fade" id="tab5default"><div class="txt_body_winner"> Default 5</div></div>
-                    </div>
-                </div>
-             </div>
+    <div class="row">
+            <div class="col-xs-12  col-sm-12 col-md-12 col-lg-12">
+                <img src="images/winner/winner.png" class="img-responsive center-block">
+            </div>
         </div>
-        <div class="col-xs-12 col-sm-12 col-md-1 col-lg-2 "> </div>             
+   <div class="row ">
+       <div class="bg_winner">
+            <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12 ">
+                <div class="panel with-nav-tabs panel-default">
+                    <div class="no_border panel-heading ">
+                            <ul class="nav nav-tabs">
+                                <li style="right:  -78">
+                                    <a href="#tab1default" data-toggle="tab" class="menu-link h_b">
+                                         <img src="images/winner/bar_new_01.png" class="img-responsive"> 
+                                    </a>
+                                 </li>
+                                <li style="right:  -39.5">
+                                    <a href="#tab2default" data-toggle="tab" class="menu-link h_b">
+                                         <img src="images/winner/bar_new_02.png" class="img-responsive"> 
+                                    </a>
+                                </li>
+                                <li style="">
+                                    <a href="#tab3default" data-toggle="tab" class="menu-link h_b">
+                                        <img src="images/winner/bar_new_03.png" class="img-responsive"> 
+                                    </a>
+                                </li>
+                                 <li style="left: -39;">
+                                     <a href="#tab4default" data-toggle="tab" class="menu-link h_b">
+                                       <img src="images/winner/bar_new_04.png" class="img-responsive"> 
+                                     </a>
+                                 </li>
+                                  <li style="left: -79;">
+                                     <a href="#tab5default" data-toggle="tab" class="menu-link h_b">
+                                       <img src="images/winner/bar_new_05.png" class="img-responsive"> 
+                                     </a>
+                                 </li>
+                            </ul>
+                    </div>
+                    <div class="panel-body">
+                        <div class="tab-content">
+                            <div class="tab-pane fade in active" id="tab1default"><div class="txt_body_winner"> Default 111<br><br><br><br><br></div></div>
+                            <div class="tab-pane fade" id="tab2default"><div class="txt_body_winner"> Default 2</div></div>
+                            <div class="tab-pane fade" id="tab3default"><div class="txt_body_winner"> Default 3</div></div>
+                            <div class="tab-pane fade" id="tab4default"><div class="txt_body_winner"> Default 4</div></div>
+                            <div class="tab-pane fade" id="tab5default"><div class="txt_body_winner"> Default 5</div></div>
+                        </div>
+                    </div>
+                 </div>
+            </div>    
+       </div>
    </div>
  </div>
             <!--</div>
