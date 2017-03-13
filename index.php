28b6ea5349ad60c3ef998b57fc74dadcd7ac2fdc
chinoros <chinoros.x@gmail.com>
2017-03-13 10:52:29 +0700

    add hello

--- index.php
+++ index.php
@@ -37,11 +37,144 @@
       <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
       <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
     <![endif]-->
-   
-    
-  </head>
+    <style>
+        /*.r_bg{   
+            background-image: url("images/rule/r_bg.png") ;
+            background-repeat: no-repeat;
+            background-size: 100% auto ;
+            background-position: center center;
+             -webkit-background-size: cover;
+            -moz-background-size: cover;
+            -o-background-size: cover;
+            background-size: cover;
+        }*/
+        body { 
+            background: url(images/rule/r_bg.png) no-repeat center top local; 
+            -webkit-background-size: cover;
+            -moz-background-size: cover;
+            -o-background-size: cover;
+            background-size: cover;
+         }
+         
+         .bottom0.panel-group  {
+             margin-bottom: 0;
+            }
 
-  <body class="body">
+            .notifications-scroll-area {
+                height: 550px;
+                position: relative;
+                overflow: auto;
+            }
+            .bg_footer1{
+                
+                background: url(images/main/m_bg_pop.png) no-repeat center top local; 
+                -webkit-background-size: cover;
+                -moz-background-size: cover;
+                -o-background-size: cover;
+                background-size: cover;
+                background-color: rgba(51, 122, 183, 0);
+                width: 100%;
+                
+            }
+            .bg_footer2{
+                //background-color: #000;
+                width: 100%;
+                height:  10em;
+                background: url(images/main/m_footer2.png) no-repeat center top local; 
+                -webkit-background-size: cover;
+                -moz-background-size: cover;
+                -o-background-size: cover;
+                background-size: cover;
+            }
+            
+            .bu_footer{
+                width: 80%;
+               // height:  100%;
+                background: url(images/main/m_b_footer.png) no-repeat center top local; 
+                -webkit-background-size: cover;
+                -moz-background-size: cover;
+                -o-background-size: cover;
+                background-size: cover;
+            }
+            
+            .panel-primary > .panel-heading {
+                color: rgba(51, 122, 183, 0);
+                background-color: rgba(51, 122, 183, 0);
+                border-color: rgba(51, 122, 183, 0);
+            }
+            .panel-heading {
+                padding: 50px 15px;
+                border-bottom: 0px solid transparent;
+                border-top-left-radius: 3px;
+                border-top-right-radius: 3px;
+            }
+            .panel-primary {
+                border-color: rgba(253, 254, 255, 0);
+            }
+            
+            .panel {
+                margin-bottom: 20px;
+                background-color: #fff;
+                border: 1px solid transparent;
+                border-radius: 4px;
+                -webkit-box-shadow: 0 1px 1px rgba(0, 0, 0, .00);
+                box-shadow: 0 1px 1px rgba(0, 0, 0, 0);
+            }
+            
+            .panel-group .panel-heading + .panel-collapse > .panel-body, .panel-group .panel-heading + .panel-collapse > .list-group {
+                border-top: 0px solid #ddd; 
+            }
+            
+            .panel {
+                margin-bottom: 20px;
+                background-color: rgba(247, 196, 196, 0);
+                border: 1px solid transparent;
+                border-radius: 4px;
+                -webkit-box-shadow: 0 1px 1px rgba(0, 0, 0, .00);
+                box-shadow: 0 1px 1px rgba(0, 0, 0, 0);
+            }
+            
+            .txt_r{
+               font-family: "db_helvethaica_x";
+          
+               display: block;
+            
+            }
+            
+            
+            .r_body{
+                color: #000;
+                font-size:1.8em;
+                font-weight: normal;
+                display: inline;
+            }
+             .box_txt{
+                 background: url(images/main/m_bpx_txt.png) no-repeat center top local; 
+                 height: 100%;
+                -webkit-background-size: cover;
+                -moz-background-size: cover;
+                -o-background-size: cover;
+                background-size: cover;
+             
+            }
+            
+            .notifications-scroll-area {
+               height: auto;
+                /* position: relative; */
+                /* overflow: auto; */
+            }
+    </style>
+    
+      <!-- The Boutique CSS: -->
+        <link rel="stylesheet" href="css/boutique.css">
+
+        <!-- The Boutique plugin: -->
+        <script src="js/jquery.boutique.min.js"></script>
+        
+        
+  </head>
+<!--<body class="r_bg">-->
+  <body >
         <div class="container conter2">
             <!-- Static navbar -->
             <nav class="navbar navbar-default navbar2" role="navigation">
@@ -59,30 +192,193 @@
                   <ul class="nav navbar-nav">
                     <li><a href="#" class="txt_menu">หน้าแรก</a></li>
                     <li><a href="#" class="txt_menu">กติกาและของรางวัล</a></li>
-                    <li><a href="#" class="txt_menu">ส่งผลงาน</a></li>
+                     <li><a href="#" class="txt_menu">เลือกฮีโร่</a></li>
                   </ul>
                   <ul class="nav navbar-nav navbar-right menu_top_right">
-                    <li><a href="#" class="txt_menu">SHOWCASE</a></li>
-                    <li ><a href="#" class="txt_menu">ประกาศผู้โชคดี</a></li>
-                    <li><a href="#" class="txt_menu">ร่วมกิจกรรม</a></li>
+                    <li><a href="#" class="txt_menu">โหวตภาพประกวด</a></li>
+                    <li><a href="#" class="txt_menu">ประกาศผู้โชคดี</a></li>
+                    <li><a href="#" class="txt_menu">ส่งผลงาน</a></li>
                   </ul>
                 </div><!--/.nav-collapse -->
               </div>
             </nav>
-             <div id="content">
-                 <div class="row home_body">
-                    <div class="col-xs-12 col-sm-12 col-md-12 col-lg-4 ">
-                        <img src="./images/home_body_right.png" class="home_image_right img-responsive center-block"/>
-                    </div>
-
-                    <div class="col-xs-12 col-sm-12 col-md-12 col-lg-6 col-lg-offset-1">
-                      <img src="./images/home_body_left.png" class=" img-responsive center-block"/>
-                   </div>
-                </div>
-                 <!--#include virtual="/page/index.html" -->
-            </div>
         </div>
+        <div id="content">
+            <div class="row home_body">
+                <div class="col-xs-12 col-sm-12 col-md-12 col-lg-1 ">
 
+                </div>
+               <div class="col-xs-12 col-sm-12 col-md-12 col-lg-4 ">
+                   <img src="./images/home_body_right.png" class="home_image_right img-responsive center-block"/>
+               </div>
+
+               <div class="col-xs-12 col-sm-12 col-md-12 col-lg-5 col-lg-offset-1">
+                 <img src="./images/home_body_left.png" class=" img-responsive center-block"/>
+              </div>
+           </div>
+            <!--#include virtual="/page/index.html" -->
+         </div>
+      <!--
+        <div class="footer navbar-fixed-bottom " >
+            <div class="">
+                <div class="panel-group bottom0 bg_footer1" id="accordion" role="tablist" aria-multiselectable="true">
+                    <div class="panel panel-primary ">
+                      <div class="panel-heading bu_footer center-block" role="tab" id="headingOne" style="cursor: pointer" data-toggle="collapse" data-parent="#accordion" href="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+                       <h4 class="panel-title">
+                          ccc
+                              
+                        </h4>
+                        
+                          
+                      </div>
+                      <div id="collapseOne" class="panel-collapse collapse notifications-scroll-area" role="tabpanel" aria-labelledby="headingOne">
+                        <div class="panel-body ">
+                            <div id="content">
+                               <div class="row">
+                                   <div class="col-xs-12 col-sm-12 col-md-1 col-lg-1 ">
+
+                                   </div>
+                                   <div class="col-xs-12 col-sm-12 col-md-5 col-lg-5 ">
+                                       <row>
+                                           <div class="col-xs-12 col-sm-12 col-md-6 col-lg-6 ">
+                                                <img src="images/main/m_join_t_1.png" class="img-responsive">
+                                           </div>
+                                           <div class="col-xs-12 col-sm-12 col-md-6 col-lg-6 ">
+                                               <img src="images/main/m_join_i_1.png" class="img-responsive">
+                                           </div>
+                                       </row>
+                                       <row>
+                                           <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12 ">
+                                               <ol class="ol_left">
+                                                  <li>
+                                                      <div class="txt_r r_body">
+                                                          คลิกที่หัวข้อ “เลือกฮีโร่”
+                                                      </div>
+                                                  </li>
+                                                   <li>
+                                                      <div class="txt_r r_body">
+                                                          เลือกประเภทอายุน้อยกว่า 8 ปี ( เกิดวันที่ 1 มี.ค. 2552 เป็นต้นไป )
+                                                      </div>
+                                                  </li>
+                                                    <li>
+                                                      <div class="txt_r r_body">
+                                                          เลือกดาวน์โหลดฮีโร่ที่ต้องการระบายสีส่งประกวด
+                                                      </div>
+                                                  </li>
+                                                  <li>
+                                                      <div class="txt_r r_body">
+                                                           พิมพ์ภาพที่เลือก แล้วนำมาระบายสีหรือแต่งเติม ตามความคิดสร้างสรรค์       
+                                                      </div>
+                                                  </li>
+                                                  <li>
+                                                      <div class="txt_r r_body">
+                                                          หลังเสร็จสมบูรณ์ ถ่ายรูปหรือสแกนผลงาน แล้วอัพโหลดที่หน้าส่งผลงาน
+                                                      </div>
+                                                  </li>
+                                              </ol>
+                                           </div>
+                                       </row>
+                                   </div>
+                                   <div class="col-xs-12 col-sm-12 col-md-1 col-lg-1 ">
+
+                                   </div>
+                                   <div class="col-xs-12 col-sm-12 col-md-5 col-lg-5 ">
+                                       <row>
+                                           <div class="col-xs-12 col-sm-12 col-md-6 col-lg-6 ">
+                                               <img src="images/main/m_join_t_2.png" class="img-responsive"> 
+                                           </div>
+                                           <div class="col-xs-12 col-sm-12 col-md-6 col-lg-6 ">
+                                               <img src="images/main/m_join_i_2.png" class="img-responsive">
+                                           </div>  
+                                       </row>
+                                       <row>
+                                           <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12 ">
+                                               <ol >
+                                                   <li>
+                                                       <div class="txt_r r_body">
+                                                           คลิกที่หัวข้อ “เลือกฮีโร่”
+                                                       </div>
+                                                   </li>
+                                                    <li>
+                                                       <div class="txt_r r_body">
+                                                          เลือกประเภทอายุ 8 -12 ป ( เกิดในระหว่างวันที่ 1 มี.ค. 2548 - 2 ก.พ. 2552 )
+                                                       </div>
+                                                   </li>
+                                                     <li>
+                                                       <div class="txt_r r_body">
+                                                           เลือกชื่อฮีโร่ และอ่านคำอธิบายลักษณะฮีโร่ที่ต้องการส่งประกวด
+                                                       </div>
+                                                   </li>
+                                                   <li>
+                                                       <div class="txt_r r_body">
+                                                            วาดภาพฮีโร่ตามความคิดสร้างสรรค์ให้มีลัษณะตามโจทย์      
+                                                       </div>
+                                                   </li>
+                                                   <li>
+                                                       <div class="txt_r r_body">
+                                                           หลังเสร็จสมบูรณ์  ถ่ายรูปหรือสแกนผลงาน แล้วอัพโหลดไฟล์ที่หน้าส่งผลงาน
+                                                       </div>
+                                                   </li>
+                                               </ol>
+                                           </div>
+                                       </row>
+                                   </div>
+                                </div>
+                                
+                                <div class="row">
+                                    <div class="col-xs-12 col-sm-12 col-md-12 col-lg-3"></div>
+                                    <div class="col-xs-12 col-sm-12 col-md-12 col-lg-6 ">
+                                        <div class="box_txt">
+                                            <div>
+                                                <ol class="center-block">
+                                                    <li>
+                                                        <div class="txt_r r_body">
+                                                            คลิกเข้าไปที่หัวข้อ “ภาพส่งประกวด”
+                                                        </div>
+                                                    </li>
+                                                     <li>
+                                                        <div class="txt_r r_body">
+                                                           เลือกรุ่นอายุที่ต้องการโหวต
+                                                        </div>
+                                                    </li>
+                                                      <li>
+                                                        <div class="txt_r r_body">
+                                                            กด Vote ผลงานที่ถูกใจ โดย สามารถกดโหวตได้ผลงานละ 1 ครั้ง แต่สามารถโหวตกี่ผลงานก็ได้
+                                                        </div>
+                                                    </li>
+                                                    <li>
+                                                        <div class="txt_r r_body">
+                                                             ผู้ที่ร่วมโหวตมีสิทธิ์ลุ้นรับรางวัลพิเศษ      
+                                                        </div>
+                                                    </li>
+                                                </ol>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="col-xs-12 col-sm-12 col-md-12 col-lg-3"></div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="bg_footer2">
+                  <!--<div class="panel-group bottom0" id="accordion" role="tablist" aria-multiselectable="true">
+                    <div class="panel panel-primary">
+                      <div class="panel-heading" role="tab" id="headingOne" style="cursor: pointer" data-toggle="collapse" data-parent="#accordion" href="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+                        <h4 class="panel-title">
+                                          <i class="fa fa-info"></i>&nbsp;&nbsp;Notifications 
+                                      </h4>
+                      </div>
+                      <div id="collapseOne" class="panel-collapse collapse notifications-scroll-area" role="tabpanel" aria-labelledby="headingOne">
+                        <div class="panel-body ">
+                         xxx
+                        </div>
+                      </div>
+                    </div>
+                  </div>-->
+             <!-- </div>
+        </div>-->
     
 
 
