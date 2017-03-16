975b3a5b16705fcb87f5158529ea41ff3a604595
chinoros <chinoros.x@gmail.com>
2017-03-16 10:23:32 +0700

    add

---
+++ index_10.php
@@ -0,0 +1,380 @@
+<html lang="en" >
+    <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="icon" href="../favicon.ico">
+
+    <title>Blackmore Our Strong Family</title>
+
+    <!-- Bootstrap core CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+    <!--<link href="css/bootstrap.css" rel="stylesheet">-->
+
+    <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+    <link href="css/ie10-viewport-bug-workaround.css" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+
+    <link href="css/font.css" rel="stylesheet">
+    
+    <link href="css/theme_web.css" rel="stylesheet">
+    <script src="https://code.jquery.com/jquery-1.12.4.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/theme.js"></script>  
+   
+    <!-- Just for debugging purposes. Don't actually copy these 2 lines! -->
+    <!--[if lt IE 9]><script src="js/ie8-responsive-file-warning.js"></script><![endif]-->
+    <script src="js/ie-emulation-modes-warning.js"></script>
+
+    <script src="https://cdn.jsdelivr.net/jquery.validation/1.15.1/jquery.validate.min.js"></script>
+    <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
+    
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+    <style>
+        /*.r_bg{   
+            background-image: url("images/rule/r_bg.png") ;
+            background-repeat: no-repeat;
+            background-size: 100% auto ;
+            background-position: center center;
+             -webkit-background-size: cover;
+            -moz-background-size: cover;
+            -o-background-size: cover;
+            background-size: cover;
+        }*/
+        
+        /** rule */
+        /*body { 
+            background: url(images/rule/r_bg.png) no-repeat center top local; 
+            -webkit-background-size: cover;
+            -moz-background-size: cover;
+            -o-background-size: cover;
+            background-size: cover;
+         }*/
+        
+        /** main */
+        body { 
+            background: url(images/main2/main_bg_1.jpg) no-repeat center top local; 
+            -webkit-background-size: cover;
+            -moz-background-size: cover;
+            -o-background-size: cover;
+            background-size: cover;
+         }
+
+         
+         
+            .bottom0.panel-group  {
+             margin-bottom: 0;
+            }
+
+            .notifications-scroll-area {
+                height: 550px;
+                position: relative;
+                overflow: auto;
+            }
+            .bg_footer1{
+                
+                background: url(images/main2/m_bg_pop.png) no-repeat center top local; 
+                -webkit-background-size: cover;
+                -moz-background-size: cover;
+                -o-background-size: cover;
+                background-size: 80% 80%;
+                background-color: rgba(51, 122, 183, 0);
+                width: 100%;
+                
+            }
+            .bg_footer2{
+                //background-color: #000;
+                width: 100%;
+                height:  10em;
+                background: url(images/main2/m_footer2.png) no-repeat center top local; 
+                -webkit-background-size: cover;
+                -moz-background-size: cover;
+                -o-background-size: cover;
+                background-size: cover;
+            }
+            
+            .bu_footer{
+                width: 80%;
+               // height:  100%;
+                background: url(images/main/m_b_footer.png) no-repeat center top local; 
+                -webkit-background-size: cover;
+                -moz-background-size: cover;
+                -o-background-size: cover;
+                background-size: cover;
+            }
+            
+            .panel-primary > .panel-heading {
+                color: rgba(51, 122, 183, 0);
+                background-color: rgba(51, 122, 183, 0);
+                border-color: rgba(51, 122, 183, 0);
+            }
+            .panel-heading {
+                padding: 50px 15px;
+                border-bottom: 0px solid transparent;
+                border-top-left-radius: 3px;
+                border-top-right-radius: 3px;
+            }
+            .panel-primary {
+                border-color: rgba(253, 254, 255, 0);
+            }
+            
+            .panel {
+                margin-bottom: 20px;
+                background-color: #fff;
+                border: 1px solid transparent;
+                border-radius: 4px;
+                -webkit-box-shadow: 0 1px 1px rgba(0, 0, 0, .00);
+                box-shadow: 0 1px 1px rgba(0, 0, 0, 0);
+            }
+            
+            .panel-group .panel-heading + .panel-collapse > .panel-body, .panel-group .panel-heading + .panel-collapse > .list-group {
+                border-top: 0px solid #ddd; 
+            }
+            
+            .panel {
+                margin-bottom: 20px;
+                background-color: rgba(247, 196, 196, 0);
+                border: 1px solid transparent;
+                border-radius: 4px;
+                -webkit-box-shadow: 0 1px 1px rgba(0, 0, 0, .00);
+                box-shadow: 0 1px 1px rgba(0, 0, 0, 0);
+            }
+            
+            .txt_r{
+               font-family: "db_helvethaica_x";
+          
+               display: block;
+            
+            }
+            
+            
+            .r_body{
+                color: #000;
+                font-size:1.8;
+                font-weight: normal;
+                display: inline;
+            }
+             .box_txt{
+                 background: url(images/main2/m_bpx_txt.png) no-repeat center top local; 
+                 height: 100%;
+                -webkit-background-size: cover;
+                -moz-background-size: cover;
+                -o-background-size: cover;
+                background-size: cover;
+             
+            }
+            
+            .notifications-scroll-area {
+               height: auto;
+                /* position: relative; */
+                /* overflow: auto; */
+            }
+            
+           
+            
+            @media (max-width: 768px) { 
+            /*moblie*/
+                
+                .container {
+                    padding-right: 15px;
+                    padding-left: 50px; 
+                    margin-right: auto;
+
+                } 
+                
+            }
+            
+             @media (max-width: 480px) { 
+            /*moblie*/
+                /*.test{
+                    //background-color: #000;
+                    width :500px;
+                    height: 500px;
+                }*/
+                .txt_menu{
+                    font-family: "helvethaica_x";
+                    text-align: center;    
+                    font-size: 1em;
+                    color: #ffffff;
+                }
+    
+                .container {
+                    padding-right: 0px;
+                    padding-left: 0px; 
+                    margin-right: auto;
+                    margin-left: auto;
+
+                } 
+                .txt_style{
+                    
+                }
+                
+                .navbar-default {
+                    background-color: #36A1CA;
+                    border-color: #rgb(231, 231, 231, 0);
+                }
+                
+                .logo{
+                    background-image: url("images/mobile/logo.png");
+                    background-image-size: 100% 230px;
+                    background-size: 100% 100%;
+                    border-radius: 0px;
+                   /* height: 230px;
+                    top: 20px;
+                    align-content: left;
+                    display: block;*/
+                }
+                
+                .send_work{
+                    background-image: url("images/mobile/send_work.png");
+                    background-size: 86px 70px;
+                    width: 86px;
+                    height: 70px;
+                    
+                    /*background-color: #000;
+                    width: 100px;*/
+                }
+                .txt_bu_m_mobile{
+                   color: #ffffff;
+                }
+                
+                .container-fluid>.navbar-collapse, .container-fluid>.navbar-header, .container>.navbar-collapse, .container>.navbar-header {
+                    margin-right: 0px; 
+                    margin-left: 0px;
+                }
+                
+                .navbar-default .navbar-toggle:focus, .navbar-default .navbar-toggle:hover {
+                     background-color: #36a1ca;
+                }
+                
+                .navbar-default .navbar-toggle {
+                    border-color: #36a1ca;
+                }
+                
+                .show_img{
+                    width: 90%;
+                }
+             }
+    </style>
+    
+      <!-- The Boutique CSS: -->
+        <link rel="stylesheet" href="css/boutique.css">
+
+        <!-- The Boutique plugin: -->
+        <script src="js/jquery.boutique.min.js"></script>
+        
+        
+  </head>
+<!--<body class="r_bg">-->
+  <body>
+        <div class="container conter2 ">
+            <!-- Static navbar -->
+            <nav class="navbar navbar-default navbar2" role="navigation">
+              <div class="container menu_top_text">
+                <div class="navbar-header">
+                  <button type="button" class="navbar-toggle collapsed txt_bu_m_mobile" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+                    <!--<span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>-->
+                      BLACKMORES
+                  </button>
+
+                </div>
+                <div id="navbar" class=" navbar-collapse collapse">
+                  <ul class="nav navbar-nav">
+                    <li><a href="#" class="txt_menu txt_style" >หน้าแรก</a></li>
+                    <li><a href="#" class="txt_menu txt_style">กติกาและของรางวัล</a></li>
+                     <li><a href="#" class="txt_menu txt_style">เลือกฮีโร่</a></li>
+                  </ul>
+                  <ul class="nav navbar-nav navbar-right menu_top_right">
+                    <li><a href="#" class="txt_menu">โหวตภาพประกวด</a></li>
+                    <li><a href="#" class="txt_menu">ประกาศผู้โชคดี</a></li>
+                    <li><a href="#"> <div class="send_work center-block"></div></a><a href="#" class="txt_menu sendwork center-block">ส่งผลงานนะ</a></li>
+                  </ul>
+                </div><!--/.nav-collapse -->
+              </div>
+            </nav>
+           
+        </div>
+       <div id="content">
+           <div class="row home_body">
+                <div class="col-xs-12 col-sm-12 col-md-12 col-lg-1 ">
+
+                </div>
+               <div class="col-xs-12 col-sm-12 col-md-12 col-lg-4 ">
+                   <img src="./images/home_body_right.png" class="home_image_right img-responsive center-block"/>
+               </div>
+
+               <div class="col-xs-12 col-sm-12 col-md-12 col-lg-5 col-lg-offset-1">
+                 <img src="./images/home_body_left.png" class=" img-responsive center-block"/>
+              </div>
+           </div>
+            <!--#include virtual="/page/index.html" -->
+         </div>
+        
+     
+        <div class="footer navbar-fixed-bottom" >
+                <div class="panel-group bottom0 bg_footer1" id="accordion" role="tablist" aria-multiselectable="true">
+                    <div class="panel panel-primary ">
+                      <div class="panel-heading bu_footer center-block" role="tab" id="headingOne" style="cursor: pointer" data-toggle="collapse" data-parent="#accordion" href="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+                       <h4 class="panel-title">
+                        </h4>
+                        
+                          
+                      </div>
+                      <div id="collapseOne" class="panel-collapse collapse notifications-scroll-area" role="tabpanel" aria-labelledby="headingOne">
+                        <div class="panel-body ">
+                            <div id="content">
+                               <div class="row">
+                                   <div class="col-xs-12 col-sm-12 col-md-1 col-lg-1 ">
+ssssssssssssssssssssssssss
+                                   </div>
+                                   
+                                </div>
+                                
+                                
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="bg_footer2">
+                  <div class="panel-group bottom0" id="accordion" role="tablist" aria-multiselectable="true">
+                    <div class="panel panel-primary">
+                      <div class="panel-heading" role="tab" id="headingOne" style="cursor: pointer" data-toggle="collapse" data-parent="#accordion" href="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+                        <h4 class="panel-title">
+                                          <i class="fa fa-info"></i>&nbsp;&nbsp;Notifications 
+                                      </h4>
+                      </div>
+                      <div id="collapseOne" class="panel-collapse collapse notifications-scroll-area" role="tabpanel" aria-labelledby="headingOne">
+                        <div class="panel-body ">
+                         xxx
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+             </div>
+        </div>
+       
+
+
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    
+   
+    <!-- IE10 viewport hack for Surface/des    <script src="js/bootstrap.min.js"></script>ktop Windows 8 bug -->
+    <script src="js/ie10-viewport-bug-workaround.js"></script>
+
+
+    </body>
+</html>
