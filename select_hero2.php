e97245fb81f03e795dce4bee8e1e9a260e927bea
chinoros <chinoros.x@gmail.com>
2017-03-18 17:12:57 +0700

    no message

---
+++ select_hero2.php
@@ -0,0 +1,17 @@
+<html>
+    <head>
+        <!----function send url  ---->
+        <script>
+            var newURL = window.location.protocol + "//" + window.location.host + "/" + window.location.pathname;
+            var pathArray = window.location.pathname.split( '/' );
+            
+            var url = window.location.protocol + "//" + window.location.host +"/"+pathArray[1]+"/index.php?page="+pathArray[2];
+           window.location= url;
+           alert(url);
+        </script>
+         <!---- ---->
+    </head>
+    <body>
+        
+    </body>
+</html>
